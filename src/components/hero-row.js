ad9123577269a69cc299200be936e3c39735f65d
German Monzon <german.monzon@wnco.com>
2020-03-31 21:06:42 -0300

    Add flag icons

--- src/components/hero-row.js
+++ src/components/hero-row.js
@@ -4,6 +4,10 @@ import Avatar from '@material-ui/core/Avatar';
 import SvgIcon from '@material-ui/core/SvgIcon';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import { ReactComponent as ComicIcon } from './assets/comic.svg';
+import { ReactComponent as SeriesIcon } from './assets/series.svg';
+import { ReactComponent as EventIcon } from './assets/event.svg';
+import { ReactComponent as StoryIcon } from './assets/story.svg';
 
 function HeroRow({ hero, classes }) {
   return (
@@ -13,24 +17,17 @@ function HeroRow({ hero, classes }) {
         <h3 className={classes.heroName}>{hero.name}</h3>
       </TableCell>
       <TableCell align="right" className={classes.flagCell}>
-        <SvgIcon viewBox="0 0 480 480">
-          <g>
-            <g id="Speech_Bubble_49_">
-              <g>
-                <g>
-                  <path d="m445.62 299.38 66.19-267.02-255.9-16.16-255.91-16.16 61.12 363.42 113.57-18.93-7.95 29.57 40.6 26.15-14.65 111.52 63.22-60.15 80.61-76.69-54.02-41.14 3.66-7.83z" fill="#ff8fb8"/>
-                </g>
-              </g>
-            </g>
-            <path d="m286.16 325.96-3.66 7.83 54.02 41.14-80.61 76.69v-435.42l255.9 16.16-66.19 267.02z" fill="#ff5f96"/>
-          </g>
-        </SvgIcon>
+        <SvgIcon component={ComicIcon} viewBox="0 0 480 480" />
+      </TableCell>
+      <TableCell align="right" className={[classes.flagCell, classes.seriesIcon]}>
+        <SvgIcon component={SeriesIcon} viewBox="0 0 480 480" />
       </TableCell>
       <TableCell align="right" className={classes.flagCell}>
-        {'Se'}
+        <SvgIcon component={EventIcon} viewBox="0 0 480 480" />
       </TableCell>
-      <TableCell align="right" className={classes.flagCell}>{'E'}</TableCell>
-      <TableCell align="right" className={classes.flagCell}>{'St'}</TableCell>
+      <TableCell align="right" className={classes.flagCell}>
+        <SvgIcon component={StoryIcon} viewBox="0 0 480 480" />
+      </TableCell>
     </TableRow>
   )
 }
@@ -52,5 +49,8 @@ export default withStyles({
   flagCell: {
     lineHeight: 0,
     width: '24px'
+  },
+  seriesIcon: {
+    color: 'purple'
   }
 })(HeroRow);
