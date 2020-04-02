aa14b96228d5681fefcb7efe17a7a6cd171a8791
German Monzon <german.monzon@wnco.com>
2020-04-02 05:16:08 -0300

    Updates

---
+++ precache-manifest.1d0ffab192e13b0c4e613a72ddc0d8e6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b80be9f2e2ddd31b80020db1f0c542",
+    "url": "/marvel-master-detail/index.html"
+  },
+  {
+    "revision": "86d89bdad732b0454aef",
+    "url": "/marvel-master-detail/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2e2a03e8dbebb3ae8950",
+    "url": "/marvel-master-detail/static/js/2.8ae1f23c.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/marvel-master-detail/static/js/2.8ae1f23c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d89bdad732b0454aef",
+    "url": "/marvel-master-detail/static/js/main.cc006125.chunk.js"
+  },
+  {
+    "revision": "9a3afd98856eb9f04f70",
+    "url": "/marvel-master-detail/static/js/runtime-main.3d728730.js"
+  },
+  {
+    "revision": "eedee6e2c1143a37f4028342459ec060",
+    "url": "/marvel-master-detail/static/media/comic-disable.eedee6e2.svg"
+  },
+  {
+    "revision": "819cef8193d1edaac8caeddeb97e787c",
+    "url": "/marvel-master-detail/static/media/comic.819cef81.svg"
+  },
+  {
+    "revision": "bdf4914de7e1aa7c2c2cce1c73c58044",
+    "url": "/marvel-master-detail/static/media/event-disabled.bdf4914d.svg"
+  },
+  {
+    "revision": "b735a31de859c5152b6bdbdf25f1e715",
+    "url": "/marvel-master-detail/static/media/event.b735a31d.svg"
+  },
+  {
+    "revision": "5f117edd8baf75131d28dfa00aa33ace",
+    "url": "/marvel-master-detail/static/media/exit.5f117edd.svg"
+  },
+  {
+    "revision": "a4e6bfc7a854c6ebc18a33b694e9dc37",
+    "url": "/marvel-master-detail/static/media/search.a4e6bfc7.svg"
+  },
+  {
+    "revision": "65d7de7c6b6bfddf28213e6b5762c834",
+    "url": "/marvel-master-detail/static/media/series.65d7de7c.svg"
+  },
+  {
+    "revision": "1e9a43a9d77e1c4486edc7acaebeb924",
+    "url": "/marvel-master-detail/static/media/story-disabled.1e9a43a9.svg"
+  },
+  {
+    "revision": "859eff3c15c15fdd19fd68ccaa809f8a",
+    "url": "/marvel-master-detail/static/media/story.859eff3c.svg"
+  }
+]);
